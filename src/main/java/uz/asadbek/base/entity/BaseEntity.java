98bfb70b057ca29bc5cb2e73508e5fbea2db92d1
Asadbek Rajabboyev <rajabboyev.asadbek@uzinfocom.uz>
2026-03-18 09:44:30 +0500

    initial commit

--- src/main/java/uz/asadbek/base/entity/BaseEntity.java
+++ src/main/java/uz/asadbek/base/entity/BaseEntity.java
@@ -19,10 +19,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
-  @Id
-  @Column(name = "id")
-  private Long id;
-
   @CreatedDate
   @Column(name = "created_at", updatable = false)
   private LocalDateTime createdAt;
